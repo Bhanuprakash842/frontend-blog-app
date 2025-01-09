508b3116abf5329eedfe5b98100b83799a8f7c39
Bhanuprakash842 <127642353+Bhanuprakash842@users.noreply.github.com>
2025-01-09 23:51:34 +0530

    Comments Section

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -4,6 +4,7 @@ import BreadCrumbs from "../../components/BreadCrumbs";
 import {images} from "../../constants/index";
 import { Link } from 'react-router-dom';
 import SuggestedPosts from './container/SuggestedPosts';
+import CommentsContainer from '../../components/comments/CommentsContainer';
 
 const breadCrumbsData =[
     {
@@ -84,6 +85,7 @@ const ArticleDetailPage = () => {
               lectus proin.
             </p>
           </div>
+          <CommentsContainer className='mt-10' logginedUserId="a"/>
                 </article>
                 <SuggestedPosts header="Latest Article" posts={postsData} tags={tagsData} className="mt-8 lg:mt-0 lg:max-w-xs"/>
             </section>
