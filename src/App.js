802f8ae609a79d8b0a41721f09b1bc9957eafc67
Bhanuprakash842 <127642353+Bhanuprakash842@users.noreply.github.com>
2025-01-22 23:40:25 +0530

    login functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 import HomePage from "./pages/home/Homepage";
 import RegisterPage from "./pages/home/register/RegisterPage";
+import LoginPage from "././pages/login/Loginpage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route index path="/" element={<HomePage />} />
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
       <Toaster />
     </div>
