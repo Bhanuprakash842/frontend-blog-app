822ba01455524f6e9690df54481342617e5fc655
Bhanuprakash842 <127642353+Bhanuprakash842@users.noreply.github.com>
2025-01-22 22:04:47 +0530

    register page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { Routes,Route } from 'react-router-dom';
-import './App.css';
-import Homepage from './pages/home/Homepage';
-import ArticleDetailPage from './pages/articleDetail/ArticleDetailPage';
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
+import HomePage from "./pages/home/Homepage";
+import RegisterPage from "./pages/home/register/RegisterPage";
 
 function App() {
   return (
     <div className="App font-opensans">
       <Routes>
-        <Route index path='/' element={<Homepage/>}/>
-        <Route path="/blog/:id" element={<ArticleDetailPage/>} />
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/blog/:id" element={<ArticleDetailPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
   );
